eb172f763a5d2f0df62a1c036f7dc40acbe2472d
kradam <lovidmann@gmail.com>
2022-10-05 07:41:49 +0200

    read_todo kesz kb

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 from email.utils import parsedate
 import json
+from tokenize import group
 import requests
-import json
+
 
 TODO_API_URL = "https://jsonplaceholder.cypress.io/todos"
 
-def save_todos():
+def create_todo():
     response = requests.get(TODO_API_URL)
     with open("todos.json", "w") as f:
         json.dump(response.json(), f)
-    f.close()
+    
+create_todo()
 
-save_todos()
+def read_todo(idg):
+    f = open("todos.json", "r")
+    todos = json.load(f)
+    group_todos = {}
+    for todo in todos:
+        id = todo["userId"]    
+        if id not in group_todos:
+            group_todos[id] = []
+        group_todos[id].append(todo)
+    for todo in group_todos[idg]:
+        print(todo) 
+    
+    
 
+read_todo(3)
+    
