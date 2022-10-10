0b6cbcd6531e8fe601bfde797b87ba631c524cf3
kradam <lovidmann@gmail.com>
2022-10-10 18:21:19 +0200

    sync

--- main.py
+++ main.py
@@ -6,56 +6,49 @@ import requests
 
 TODO_API_URL = "https://jsonplaceholder.cypress.io/todos"
 
+
 def create_todo():
     response = requests.get(TODO_API_URL)
     with open("todos.json", "w") as f:
         json.dump(response.json(), f)
-    
-create_todo()
+    return response.json()
+
+
+todos = create_todo()
+
 
 def read_todo(idg):
     f = open("todos.json", "r")
     todos = json.load(f)
     group_todos = {}
     for todo in todos:
-        id = todo["userId"]    
+        id = todo["userId"]
         if id not in group_todos:
             group_todos[id] = []
         group_todos[id].append(todo)
-    for todo in group_todos[idg]:
-        print(todo) 
+
 
 read_todo(3)
-    
-def update_todo(idg):
+
+
+def update_todo(idg, file_obj):
+    for todo in file_obj:
+        if todo["id"] == idg:
+            todo["title"] = todo["title"] + '#'
+            print(todo["title"])
+
+    return file_obj
+
+
+update_todo(23, todos)
+
+
+def delete_todo(idg, file_obj):
     f = open("todos.json", "w")
-    todos = json.load(f)
-    mod_todos = {}
-    for todo in todos:
-        if todo["userId"] == idg:
-            mod_todos.append(todo)
-    #for todo in mod_todos:
-        #edit todos
-    #for todo in todos                                  #merge todos
-        #for todom in mod_todos
-            #if todo["userId"] == todom["userId"] and todo["id"] == todom["id"]:
-                #todo = todom
-    json.dump(todos, f)
-    return mod_todos
-    
-    
-
-def delete_todo(idg):
-    f = open("todos.json", "r")
-    todos = json.load(f)
-    for i in range(len(todos)):
-        if todos[i]['id'] == idg:
-            del todos[i]
-    json.dump(todos, f)
-
-delete_todo(23)
-    
+    for todo in file_obj:
+        if todo["id"] == idg:
+            file_obj.remove(todo)
+    json.dump(file_obj, f)
 
 
-
-    
+delete_todo(24, todos)
