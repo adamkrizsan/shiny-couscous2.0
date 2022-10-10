e81230df0ade41f265996bbfcaf9ebf2a47dca74
kradam <lovidmann@gmail.com>
2022-10-10 18:19:18 +0200

    asd

--- main.py
+++ main.py
@@ -24,8 +24,38 @@ def read_todo(idg):
         group_todos[id].append(todo)
     for todo in group_todos[idg]:
         print(todo) 
+
+read_todo(3)
+    
+def update_todo(idg):
+    f = open("todos.json", "w")
+    todos = json.load(f)
+    mod_todos = {}
+    for todo in todos:
+        if todo["userId"] == idg:
+            mod_todos.append(todo)
+    #for todo in mod_todos:
+        #edit todos
+    #for todo in todos                                  #merge todos
+        #for todom in mod_todos
+            #if todo["userId"] == todom["userId"] and todo["id"] == todom["id"]:
+                #todo = todom
+    json.dump(todos, f)
+    return mod_todos
     
     
 
-read_todo(3)
+def delete_todo(idg):
+    f = open("todos.json", "r")
+    todos = json.load(f)
+    for i in range(len(todos)):
+        if todos[i]['id'] == idg:
+            del todos[i]
+    json.dump(todos, f)
+
+delete_todo(23)
+    
+
+
+
     
